5cc3069139b8ea3a24b0b14c4963d1a0fcf3f8e2
fatherofsevenchildren <kudienko030303@mail.ru>
2024-09-25 23:00:14 +0300

    configured HikariDataSource

--- ORM/src/main/java/edu/school21/orm/OrmManager.java
+++ ORM/src/main/java/edu/school21/orm/OrmManager.java
@@ -1,6 +1,19 @@
 package edu.school21.orm;
 
+import edu.school21.annotations.OrmEntity;
+
+import javax.sql.DataSource;
+
 public class OrmManager {
 
+    DataSource dataSource;
+
+    public OrmManager(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void run(Class<?> clazz) {
+        System.out.println(clazz.getAnnotation(OrmEntity.class).table());
+    }
 
 }
