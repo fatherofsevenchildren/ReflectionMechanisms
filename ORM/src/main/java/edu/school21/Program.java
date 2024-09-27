51f43f35405367e863a597217884fba356b055e5
Максим Кудиенко <93645634+fatherofsevenchildren@users.noreply.github.com>
2024-09-27 12:24:21 +0300

    fix: add try catch in main method Program.java

--- ORM/src/main/java/edu/school21/Program.java
+++ ORM/src/main/java/edu/school21/Program.java
@@ -2,17 +2,19 @@ package edu.school21;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import edu.school21.models.User;
 import edu.school21.orm.OrmManager;
 
 import java.sql.SQLException;
 
-public class Program
-{
-    public static void main( String[] args ) throws SQLException {
+public class Program {
+    public static void main(String[] args) {
         HikariConfig config = new HikariConfig(
                 "/datasource.properties");
         OrmManager ormManager = new OrmManager(new HikariDataSource(config));
+        try {
             ormManager.init();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
